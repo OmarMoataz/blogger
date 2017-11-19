d21e81bed33e499b573acbf23b1cf987bf1d45d1
Omar Moataz <omarmoataz@outlook.com>
2017-11-19 23:01:24 +0200

    Added styling to the blog and configured a few controller actions.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 	def index
-		@articles = Articles.all
+		@articles = Article.all
+	end
+	def show
+		@article = Article.find(params[:id])
 	end
 end

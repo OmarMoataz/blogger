46e35b5b1ce982f1bb8fcd767f0d492742d7216a
Omar Moataz <omarmoataz@outlook.com>
2017-11-21 12:32:21 +0200

    added working delete, edit, and update features. Finished form_based_workflow.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,17 @@ class ArticlesController < ApplicationController
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
-		redirect_to article_path(@articles)
+		redirect_to articles_path(@articles)
+	end
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' Updated!"
+
+		redirect_to article_path(@article)
 	end
 end
